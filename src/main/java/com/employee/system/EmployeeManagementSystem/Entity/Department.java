a7f192ed3d06ea0d18e7b8305adae5dc2aaf193a
Ajmal <ajmalateranzhiyil@gmail.com>
2024-02-19 20:04:05 +0530

    commit

---
+++ src/main/java/com/employee/system/EmployeeManagementSystem/Entity/Department.java
@@ -0,0 +1,36 @@
+package com.employee.system.EmployeeManagementSystem.Entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "department")
+@Data
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "dept_id")
+    private int id;
+
+    @Column(name = "dept_name")
+    private String name;
+
+    @Column(name = "create_date",updatable = false)
+    @JsonProperty(value = "create_date")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    @CreationTimestamp
+    @Setter(value = AccessLevel.NONE)
+    private LocalDateTime createDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "department_head_id")
+    private Employee departmentHead;
+}
---
+++ src/main/java/com/employee/system/EmployeeManagementSystem/Entity/Employee.java
@@ -0,0 +1,71 @@
+package com.employee.system.EmployeeManagementSystem.Entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Table(name = "employee")
+@Data
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "emp_id")
+    private int id;
+
+    @Column(name = "emp_name")
+    private String name;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    @Column(name = "date_of_birth")
+    private LocalDate dateOfBirth;
+
+    @Column(name = "emp_salary")
+    private BigDecimal salary;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "department_id")
+    private Department department;
+
+    @Column(name = "emp_address")
+    private String address;
+
+    @Column(name = "emp_role")
+    private String role;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    @Column(name = "joining_date")
+    private Date joiningDate;
+
+    @Column(name = "yearly_bonus_percentage")
+    private BigDecimal yearlyBonusPercentage;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reporting_manager_id")
+    private Employee reportingManager;
+
+    @Column(name = "created_at",updatable = false)
+    @JsonProperty(value = "created_at")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    @CreationTimestamp
+    @Setter(value = AccessLevel.NONE)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at",insertable = false)
+    @JsonProperty(value = "updated_at")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    @UpdateTimestamp
+    @Setter(value = AccessLevel.NONE)
+    private LocalDateTime updatedAt;
+}
